909f3492aa3b8a305b7fd85a473759a6513d5299
sabaalavidze <sabaalavidze5@gmail.com>
2025-02-02 17:18:41 +0400

    first commit

---
+++ day 1/homework/homework 1.py
@@ -0,0 +1,11 @@
+name = "saba"
+surname = "alavidze"
+age = 20
+from_kutaisi = True
+
+print(name)
+print(surname)
+print(name + " " + surname)
+print("Age: " + str(age))
+print("From Kutaisi: " + str(from_kutaisi))
+print(f"My name is {name} {surname}, I am {age} years old, and I am from Kutaisi: {from_kutaisi}")
